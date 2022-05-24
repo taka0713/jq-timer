4f30f01892dec3552d8c9b1edeab290395fd5dc4
taka0713 <tyunisu2018@gmail.com>
2022-05-25 01:00:48 +0900

    20220525

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ $(function(){
             const minutes = $('#minutes').val();
             const seconds = $('#seconds').val();
             $('#seconds').val(seconds - 1);
+            if(seconds.value.length == 0){
+                alert('終了です');
+            }
+            if(seconds>0){}
         },1000);
+    });
+
+    $('#stop').click(function(){
+        clearInterval();
+        
+    });
+    
+    $('#reset').click(function(){
+        clearInterval(interval);
+        resetTimer();
     });
 
 });
